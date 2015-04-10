6e874b3cac9433ecb5886f04ae8e338192773810
Marcos Gabarda <marcosgabarda@gmail.com>
2015-04-10 20:21:05 +0200

    domain

--- pycones/pycones/settings/production.py
+++ pycones/pycones/settings/production.py
@@ -20,7 +20,7 @@ def get_env_setting(setting):
 
 ########## HOST CONFIGURATION
 # See: https://docs.djangoproject.com/en/1.5/releases/1.5/#allowed-hosts-required-in-production
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["2015.es.pycon.org"]
 ########## END HOST CONFIGURATION
 
 ########## EMAIL CONFIGURATION
