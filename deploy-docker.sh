f571977fbe1f7ae32c096b044d70005002648c90
Yamila Moreno <yamila.ms@gmail.com>
2016-07-24 19:06:23 +0200

    Changes for deploy

--- deploy-docker.sh
+++ deploy-docker.sh
@@ -5,7 +5,7 @@ git reset --hard origin/master
 echo "...Updating pycones-2016: Building the docker image"
 docker build -t pycones2016:1.0 -f Dockerfile .
 echo "...Updating pycones-2016: Restart service"
-docker-compose -f docker-compose.yml stop pycones2016
-docker-compose -f docker-compose.yml rm -v -f pycones2016
-docker-compose -f docker-compose.yml up -d --no-deps pycones2016
+$DOCKER_COMPOSE -f docker-compose.yml stop pycones2016
+$DOCKER_COMPOSE -f docker-compose.yml rm -v -f pycones2016
+$DOCKER_COMPOSE -f docker-compose.yml up -d --no-deps pycones2016
 echo "Updating pycones-2016: done!"
