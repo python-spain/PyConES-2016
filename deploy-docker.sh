8d0ac715c6219b139d0672cf7aedf10c9e657a3a
Yamila Moreno <yamila.ms@gmail.com>
2016-07-24 15:56:41 +0200

    Fix typo

--- deploy-docker.sh
+++ deploy-docker.sh
@@ -5,7 +5,7 @@ git fetch origin
 git reset --hard origin/master
 popd
 echo "...Updating pycones-2016: Building the docker image"
-docker build -t pycones-2016:1.0 -f Dockerfile .
+docker build -t pycones2016:1.0 -f Dockerfile .
 echo "...Updating pycones-2016: Restart service"
 docker-compose -f docker-compose.yml stop pycones2016
 docker-compose -f docker-compose.yml rm -v -f pycones2016
